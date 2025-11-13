37ef06074ad899fa7969de0234d034f8dffbd7dd
Sarah Popelier <sarah.popelier@hotmail.fr>
2025-11-13 14:26:59 +0100

    initiation du micro service Entretien

---
+++ src/main/java/fr/vehiclerental/maintenance/service/MaintenanceService.java
@@ -0,0 +1,100 @@
+package fr.vehiclerental.maintenance.service;
+
+import fr.vehiclerental.maintenance.entity.Maintenance;
+import fr.vehiclerental.maintenance.entity.ReservationDTO;
+import fr.vehiclerental.maintenance.entity.Unavailability;
+import lombok.extern.slf4j.Slf4j;
+import fr.vehiclerental.maintenance.entity.VehicleDTO;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+@Slf4j
+@Service
+
+public class MaintenanceService {
+
+    /**
+     * Methode pour appeller l'api Vehicle
+     *
+     * @param idVehicle Id du vehicule demandé
+     * @return Retourne la liste de vehicule
+    */
+    public List<VehicleDTO> requestVehicle(int idVehicle) {
+        RestTemplate restTemplate = new RestTemplate();
+        String userRequest = "http://localhost:8082/vehicles/" + idVehicle;
+        VehicleDTO[] response = restTemplate.getForObject(userRequest, VehicleDTO[].class);
+        if (response == null) {
+            return new ArrayList<>();
+        } else {
+            return Arrays.asList(response);
+        }
+    }
+
+    /**
+     * Methode pour appeller l'api Vehicle
+     *
+     * @param idVehicle Id du vehicule demandé
+     * @return Retourne la liste de vehicule
+     */
+    public List<ReservationDTO> requestReservation(int idVehicle) {
+        RestTemplate restTemplate = new RestTemplate();
+        String reservationRequest = "http://localhost:8083/reservations/vehicle/" + idVehicle;
+        ReservationDTO[] response = restTemplate.getForObject(reservationRequest, ReservationDTO[].class);
+        if (response == null) {
+            return new ArrayList<>();
+        } else {
+            return Arrays.asList(response);
+        }
+    }
+
+    public boolean typeVerificationUnavaibility(String typeVehicleUnavailability, String typeVehicleRequest) {
+        if(typeVehicleUnavailability.equals(typeVehicleRequest)){
+            return  true;
+        }
+        return false;
+    }
+
+
+/*
+    /**
+     * Methode pour crée une reservation
+     *
+     * @param reservationsDao Class DAO pour faire les requetes a la bdd
+     * @param client          Information du client
+     * @param vehicle         Information du vehicule
+     * @param informations    Information donnée depuis la requete
+     * @param priceFinal      Prix final
+
+    public void createReservation(ReservationsDao reservationsDao, ClientDTO client, VehicleDTO vehicle, Unavailability informations, int priceFinal) {
+        Reservations newReservation = new Reservations();
+        newReservation.setIdClient(client.getId());
+        newReservation.setIdVehicule(vehicle.getId());
+        newReservation.setStartReservation(informations.getStartReservation());
+        newReservation.setEndReservation(informations.getEndReservation());
+        newReservation.setEstimatedKm(informations.getEstimatedKm());
+        newReservation.setPriceReservation(priceFinal);
+        reservationsDao.save(newReservation);
+    }
+
+
+    /**
+     * Methode pour modifier une reservation
+     *
+     * @param findindReservation     Information de la reservation
+     * @param reservationBodyRequest Informations issue de reservation récuperer de la requete
+     * @param reservationsDao        Class DAO pour faire les requetes a la bdd
+
+    public void editReservation(Reservations findindReservation, Reservations reservationBodyRequest, ReservationsDao reservationsDao) {
+        findindReservation.setIdClient(reservationBodyRequest.getIdClient());
+        findindReservation.setIdVehicule(reservationBodyRequest.getIdVehicule());
+        findindReservation.setStartReservation(reservationBodyRequest.getStartReservation());
+        findindReservation.setEndReservation(reservationBodyRequest.getEndReservation());
+        findindReservation.setEstimatedKm(reservationBodyRequest.getEstimatedKm());
+        findindReservation.setPriceReservation(reservationBodyRequest.getPriceReservation());
+        reservationsDao.save(findindReservation);
+    }
+    */
+}
